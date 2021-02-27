d2471f29e4292f762abc1a7607d247544b98a906
MathewsPeter <mathewspeter27@gmail.com>
2021-02-27 16:46:58 +0530

    list functionality

--- List/README.md
+++ List/README.md
@@ -1,9 +1,12 @@
 # List
 Ordered collection of elements of same or different (data)type.
+Each element is a reference(same as pointer in C) to the element.
+So, list allows storage of any data type but is relatively slow and preferred for smaller number of elements.
 No size limit.
 
+
 append: add element at right end
-remove: remove element at an index
+remove: remove an first occurence of the element
 iteration: this is the operation that goes through each element in an order
 sort: sort a list in-place
 clear: remove all elements
--- List/list.py
+++ List/list.py
@@ -7,11 +7,37 @@ for i in range(len(l)): #iteration using length of array
     print(l[i],end=", ")
 print()
 
+l.append(9) #add value 9 at right end
 l.append(8.1) #add value 8.1 at right end
 l.append(9) #add value 9 at right end
-l.append(9) #add value 9 at right end
-l.remove(8.1) #delete value 8.1
+l.remove(9) #delete value 8.1
+for i in range(len(l)): #iteration using length of array
+    print(l[i],end=", ")
+print()
 
 print(l.count(9))
+
+l_deep = l
+l_shallow=l.copy()
+l.clear()
+print("Deep copy refers to same memory as the other:", l_deep)
+print("Shallow copy is created at a different memory:",l_shallow)
+
+intl = [2,1,4,3]
+intl.sort(reverse=True)#in-place sorting
+print(intl)
+
+
+neg = [-2,-1]
+zero = [0]
+pos = [1,2]
+num = neg + zero + pos
+print("num = ", num)
+print(num.pop(2),": ", num)
+
+
+
+
+
 
 
