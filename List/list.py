08f8a8e7efaef727f989c47a504e0f38d040873f
MathewsPeter <mathewspeter27@gmail.com>
2021-02-27 13:47:03 +0530

    List functionality

--- List/list.py
+++ List/list.py
@@ -1,15 +1,17 @@
 l = [1,2,3.3,4.5, "F", "six", [7]]
 
 for e in l: #iteration
-    print(e)
-    
+    print(e,end=", ")
+print()    
 for i in range(len(l)): #iteration using length of array
-    print(l[i])
-    
-l.append(8) #add value 8 at right end
-l.append(8.1) #add value 8 at right end
-l.remove(8) #delete value 8
+    print(l[i],end=", ")
+print()
 
-print(l.count)
+l.append(8.1) #add value 8.1 at right end
+l.append(9) #add value 9 at right end
+l.append(9) #add value 9 at right end
+l.remove(8.1) #delete value 8.1
+
+print(l.count(9))
 
 
